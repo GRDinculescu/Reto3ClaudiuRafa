6c437ba6b26e4a99ca05daecc76ab2bc8d524946
GRDinculescu <grdinculescu@gmail.com>
2025-05-13 09:43:47 +0200

    Hecho main, empezado con los submenus

--- Reto3ClaudiuRafa/src/main/Main.java
+++ Reto3ClaudiuRafa/src/main/Main.java
@@ -1,10 +1,43 @@
 package main;
 
+import util.Funciones;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sn = new Scanner(System.in);
+		Random r = new Random();
 
+		String menu = """
+				Elige opcion:
+				1. Mantenimientos
+				2. Catalogo de productos
+				3. Pedidos
+				4. Informes
+				0. Salir""";
+		int op;
+
+		while (true) {
+			op = Funciones.dimeEntero(menu, sn);
+
+			if (op == 0) {
+				break;
+			}
+
+			switch (op) {
+				case 1 -> {
+					System.out.println("\n=== Mantenimiento ===");
+					Mantenimientos.menu(sn);
+				}
+				case 2 -> System.out.println("\n=== Productos ===");
+				case 3 -> System.out.println("\n=== Pedidos ===");
+				case 4 -> System.out.println("\n=== Informes ===");
+				default -> System.out.println("\n--- Opcion invalida ---");
+			}
+		}
 	}
 
 }
--- Reto3ClaudiuRafa/src/main/Mantenimientos.java
+++ Reto3ClaudiuRafa/src/main/Mantenimientos.java
@@ -1,5 +1,53 @@
 package main;
 
-public class Mantenimientos {
+import util.Funciones;
 
+import java.util.Scanner;
+
+public class Mantenimientos {
+    public static void menu(Scanner sn){
+        String menu = """
+                Elige opcion:
+                1. Gestion de categorías
+                2. Gestion de productos
+                3. Gestion de clientes
+                0. Salir""";
+        int op;
+
+        while (true){
+            op = Funciones.dimeEntero(menu, sn);
+
+            if (op == 0){
+                return;
+            }
+
+            switch (op){
+                case 1 -> {
+                    System.out.println("=== Gestion de categorías ===");
+                    gestionCategorias(sn);
+                }
+                case 2 -> {
+                    System.out.println("=== Gestion de productos ===");
+                    gestionProductos(sn);
+                }
+                case 3 -> {
+                    System.out.println("=== Gestion de clientes ===");
+                    gestionClientes(sn);
+                }
+                default -> System.out.println("--- Opcion invalida ---");
+            }
+        }
+    }
+
+    private static void gestionCategorias(Scanner sn){
+
+    }
+
+    private static void gestionProductos(Scanner sn){
+
+    }
+
+    private static void gestionClientes(Scanner sn){
+
+    }
 }
